099888b1fdf02b6c62142143b4418db1cdbbf343
Oskar Dondalski <69727933+deadlytie@users.noreply.github.com>
2022-10-20 10:29:17 +0200

    to_hsv, to_hsi done

--- lab2.py
+++ lab2.py
@@ -20,7 +20,8 @@ class BaseImage:
         """
         inicjalizator wczytujacy obraz do atrybutu data na podstawie sciezki
         """
-        data = imread(path)
+        self.data = imread(path)
+        self.color_model = color_model
 
     def save_img(self, path: str) -> None:
         """
@@ -38,21 +39,70 @@ class BaseImage:
         """
         metoda zwracajaca warstwe o wskazanym indeksie
         """
-        return self.data[layer_id]
+        return self.data[:, :, layer_id]
 
     def to_hsv(self) -> 'BaseImage':
         """
         metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsv
         metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
         """
-        pass
+        for i in self.data:
+            for col in i:
+                red, green, blue = col[0], col[1], col[2]
+
+                M = max(red, green, blue)
+                m = min(red, green, blue)
+
+                V = M / 255
+                if(M > 0):
+                    S = 1 - (m / M)
+                else:
+                    S = 0
+
+                zm = (red ** 2 + green ** 2 + blue ** 2) - (red * green - red * blue - green * blue)
+
+                if green >= blue:
+                    H = np.cos((red - green/2-blue/2) / np.sqrt(zm)) ** (-1)
+                else:
+                    H = 360 - np.cos((red - green/2-blue/2) / np.sqrt(zm)) ** (-1)
+
+                col[0] = H * 255
+                col[1] = S * 255
+                col[2] = V * 255
+
+        self.colorModel = ColorModel.hsv
+        return self
 
     def to_hsi(self) -> 'BaseImage':
         """
         metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsi
         metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
         """
-        pass
+        for i in self.data:
+            for col in i:
+                red, green, blue = col[0], col[1], col[2]
+
+                M = max(red, green, blue)
+                m = min(red, green, blue)
+                I = (red + green + blue) / 3
+                if(M > 0):
+                    S = 1 - (m / M)
+                else:
+                    S = 0
+
+                zm = (red ** 2 + green ** 2 + blue ** 2) - (red * green - red * blue - green * blue)
+
+                if green >= blue:
+                    H = np.cos((red - green/2-blue/2) / np.sqrt(zm)) ** (-1)
+                else:
+                    H = 360 - np.cos((red - green/2-blue/2) / np.sqrt(zm)) ** (-1)
+
+                col[0] = H * 150
+                col[1] = S * 150
+                col[2] = I * 150
+
+        self.colorModel = ColorModel.hsv
+        return self
 
     def to_hsl(self) -> 'BaseImage':
         """
