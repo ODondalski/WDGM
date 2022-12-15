57e8eb1906d24409faf415580e3a0601bf1a83dc
Oskar Dondalski <69727933+deadlytie@users.noreply.github.com>
2022-12-15 10:45:35 +0100

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -6,59 +6,30 @@ from typing import Any
 
 
 class GrayScaleTransform(BaseImage):
-    def __init__(self, pixels: Any, colorModel: ColorModel) -> None:
-        super().__init__(pixels, colorModel)
+    def __init__(self, data: Any, color_model: ColorModel) -> None:
+        super().__init__(data, color_model)
 
     " metoda zwracajaca obraz w skali szarosci jako obiekt klasy BaseImage "
 
-    def fromRgbToGray(self) -> BaseImage:
+    def to_gray(self) -> BaseImage:
         R, G, B = self.get_layers()
 
-        averageR = R * 0.299
-        averageG = G * 0.587
-        averageB = B * 0.114
+        avR = R * 0.299
+        avG = G * 0.587
+        avB = B * 0.114
 
-        self.pixels = averageR + averageG + averageB
-        self.colorModel = ColorModel.gray
+        self.pixels = avR + avG + avB
+        self.color_model = ColorModel.gray
         return self
-
-    def showGrayConversions(self) -> None:
-        figure, axis = matplotlib.pyplot.subplots(1, 2)
-        axis[0].imshow(self.pixels)
-        axis[0].set_title("RGB")
-        self.fromRgbToGray()
-        axis[1].imshow(self.pixels, cmap='gray')
-        axis[1].set_title("RGB -> GRAY")
-        figure.set_figwidth(12)
-        figure.set_figheight(4)
-        matplotlib.pyplot.show()
-
-    "metoda wyświetlająca warstwę o wskazanym indeksie dla obrazu w skali szarości"
-
-    def showGrayLayer(self, layer_id: int) -> None:
-        layer = self.pixels[:, layer_id]
-        imshow(layer)
-        matplotlib.pyplot.show()
-
-    "metoda wyswietlajaca obraz znajdujacy sie w atrybucie data"
-
-    def showGrayImg(self) -> None:
-        imshow(self.pixels, cmap='gray')
-        matplotlib.pyplot.show()
-
-    """metoda zwracajaca obraz w sepii jako obiekt klasy BaseImage
-       sepia tworzona metoda 1 w przypadku przekazania argumentu alpha_beta
-       lub metoda 2 w przypadku przekazania argumentu 'w' """
-
-    def fromRgbToSepia(self, alpha_beta: tuple = (None, None), w: int = None) -> BaseImage:
-        if w is None and alpha_beta is not None and \
-                (alpha_beta[0] > 1 and alpha_beta[1] < 1 and alpha_beta[0] + alpha_beta[1] == 2):
-            self.fromRgbToGray()
-            L0, L1, L2 = self.pixels, self.pixels, self.pixels
+    
+    def to_sepia(self, alpha_beta: tuple = (None, None), w: int = None) -> BaseImage:
+        if alpha_beta[0] > 1 and alpha_beta[1] < 1 and alpha_beta[0] + alpha_beta[1] == 2:
+            self.to_gray()
+            L0, L1, L2 = self.data, self.data, self.data
             L0 = np.where(L0 * alpha_beta[0] > 255, 255, L0 * alpha_beta[0])
             L2 = np.where(L2 * alpha_beta[1] > 255, 255, L2 * alpha_beta[1])
-            self.pixels = np.dstack((L0, L1, L2)).astype('i')
-            self.colorModel = ColorModel.sepia
+            self.data = np.dstack((L0, L1, L2)).astype('i')
+            self.color_model = ColorModel.sepia
             return self
         elif w is not None and alpha_beta[0] is None and alpha_beta[1] is None:
             self.fromRgbToGray()
@@ -66,24 +37,5 @@ class GrayScaleTransform(BaseImage):
             L0 = np.where(L0 + 2 * w > 255, 255, L0 + 2 * w)
             L1 = np.where(L1 + w > 255, 255, L1 + w)
             self.pixels = np.dstack((L0, L1, L2)).astype('i')
-            self.colorModel = ColorModel.sepia
+            self.color_model = ColorModel.sepia
             return self
-        else:
-            raise Exception("You must specify either '(α and β where α > 1, β < 1 and α+β = 2)'"
-                            "or just 'w' between <20, 40> to run the method!")
-
-    def showSepiaConversions(self, alpha_beta: tuple = (None, None), w: int = None) -> None:
-        figure, axis = matplotlib.pyplot.subplots(1, 2)
-        axis[0].imshow(self.pixels)
-        axis[0].set_title("RGB")
-        self.fromRgbToSepia(alpha_beta, w)
-        axis[1].imshow(self.pixels, cmap='gray')
-        if alpha_beta[0] is not None and alpha_beta[1] is not None and w is None:
-            axis[1].set_title("RGB -> SEPIA, α = %f, β = %f" % (alpha_beta[0], alpha_beta[1]))
-        elif w is not None and alpha_beta[0] is None and alpha_beta[1] is None:
-            axis[1].set_title("RGB -> SEPIA, w = %d" % w)
-        else:
-            raise Exception("You must specify either '(α and β)' or just 'w' to run the method!")
-        figure.set_figwidth(12)
-        figure.set_figheight(4)
-        matplotlib.pyplot.show()
